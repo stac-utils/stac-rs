91abb6d29c894518657dd980c58e473ab3d51736
Pete Gadomski <pete.gadomski@gmail.com>
2023-03-06 15:09:46 -0700

    feat: set whether download directory is created

--- stac-cli/src/download.rs
+++ stac-cli/src/download.rs
@@ -4,11 +4,17 @@ use stac::{Assets, Href, Links};
 use stac_async::Downloader;
 use std::path::Path;
 
-pub async fn download<A>(assets: A, directory: impl AsRef<Path>, _: bool) -> Result<()>
+pub async fn download<A>(
+    assets: A,
+    directory: impl AsRef<Path>,
+    create_directory: bool,
+) -> Result<()>
 where
     A: Assets + Href + Links + Serialize + Clone,
 {
-    let downloader = Downloader::new(assets)?;
-    downloader.download(directory).await?;
+    Downloader::new(assets)?
+        .create_directory(create_directory)
+        .download(directory)
+        .await?;
     Ok(())
 }
