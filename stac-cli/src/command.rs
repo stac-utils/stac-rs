cda0b004cc33de1e2dd19a979ea0689f7e244010
Pete Gadomski <pete.gadomski@gmail.com>
2024-04-10 14:55:59 -0600

    feat!: add item builder
    Includes a breaking change to the Asset interface (non-optional roles).

--- stac-cli/src/command.rs
+++ stac-cli/src/command.rs
@@ -1,9 +1,43 @@
 use crate::Result;
 use clap::Subcommand;
 use stac_api::GetSearch;
+use std::path::Path;
+use url::Url;
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    /// Creates a STAC Item from an asset href.
+    Item {
+        /// The asset href.
+        href: String,
+
+        /// The item id.
+        ///
+        /// If not provided, will be inferred from the filename in the href.
+        #[arg(short, long)]
+        id: Option<String>,
+
+        /// The asset key.
+        #[arg(short, long, default_value = "data")]
+        key: String,
+
+        /// The asset roles.
+        ///
+        /// Can be provided multiple times.
+        #[arg(short, long)]
+        role: Vec<String>,
+
+        /// Allow relative paths.
+        ///
+        /// If false, paths will be canonicalized, which requires that the files actually exist on the filesystem.
+        #[arg(long)]
+        allow_relative_paths: bool,
+
+        /// Use compact representation for the output.
+        #[arg(short, long)]
+        compact: bool,
+    },
+
     /// Searches a STAC API.
     Search {
         /// The href of the STAC API.
@@ -102,6 +136,17 @@ impl Command {
     pub async fn execute(self) -> Result<()> {
         use Command::*;
         match self {
+            Item {
+                id,
+                href,
+                key,
+                role,
+                allow_relative_paths,
+                compact,
+            } => {
+                let id = id.unwrap_or_else(|| infer_id(&href));
+                crate::commands::item(id, href, key, role, allow_relative_paths, compact)
+            }
             Search {
                 href,
                 max_items,
@@ -139,5 +184,20 @@ impl Command {
             Sort { href, compact } => crate::commands::sort(&href, compact).await,
             Validate { href } => crate::commands::validate(href.as_deref()).await,
         }
+    }
+}
+
+fn infer_id(href: &str) -> String {
+    if let Ok(url) = Url::parse(href) {
+        url.path_segments()
+            .and_then(|path_segments| path_segments.last())
+            .and_then(|path_segment| Path::new(path_segment).file_stem())
+            .map(|file_stem| file_stem.to_string_lossy().into_owned())
+            .unwrap_or_else(|| href.to_string())
+    } else {
+        Path::new(href)
+            .file_stem()
+            .map(|file_stem| file_stem.to_string_lossy().into_owned())
+            .unwrap_or_else(|| href.to_string())
     }
 }
---
+++ stac-cli/src/commands/item.rs
@@ -0,0 +1,24 @@
+use crate::Result;
+use stac::{item::Builder, Asset};
+
+pub fn item(
+    id: String,
+    href: String,
+    key: String,
+    roles: Vec<String>,
+    allow_relative_paths: bool,
+    compact: bool,
+) -> Result<()> {
+    let mut asset = Asset::new(href);
+    asset.roles = roles;
+    let item = Builder::new(id)
+        .asset(key, asset)
+        .canonicalize_paths(!allow_relative_paths)
+        .into_item()?;
+    if compact {
+        println!("{}", serde_json::to_string(&item)?);
+    } else {
+        println!("{}", serde_json::to_string_pretty(&item)?);
+    }
+    Ok(())
+}
