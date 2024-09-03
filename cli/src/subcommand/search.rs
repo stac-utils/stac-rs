240156fc03efc07bc4b43f02e3f9cbf55911e7d2
Pete Gadomski <pete.gadomski@gmail.com>
2024-09-03 13:35:01 -0600

    feat(cli): add duckdb searching

--- cli/src/subcommand/search.rs
+++ cli/src/subcommand/search.rs
@@ -24,40 +24,66 @@ impl Subcommand {
             items: get_items,
         };
         let search = get_search.try_into()?;
-        let client = ApiClient::new(&args.href)?;
-        let item_stream = client.search(search).await?;
-        tokio::pin!(item_stream);
-        let mut num_items = 0;
-        if args.stream {
-            while let Some(result) = item_stream.next().await {
-                let item: Item = result?;
-                num_items += 1;
-                sender.send(item.into()).await?;
-                if args
-                    .max_items
-                    .map(|max_items| num_items >= max_items)
-                    .unwrap_or(false)
-                {
-                    break;
+        if args
+            .duckdb
+            .unwrap_or_else(|| stac::geoparquet::has_extension(&args.href))
+        {
+            #[cfg(feature = "duckdb")]
+            {
+                let client = stac_duckdb::Client::from_path(args.href)?;
+                let items = client.search_to_json(search)?;
+                if args.stream {
+                    for item in items.items {
+                        sender.send(item.into()).await?;
+                    }
+                } else {
+                    sender.send(serde_json::to_value(items)?.into()).await?;
+                }
+            }
+            #[cfg(not(feature = "duckdb"))]
+            {
+                if args.duckdb == Some(true) {
+                    return Err(crate::Error::Custom(format!("`--duckdb true` was provided, but this crate was not compiled with duckdb support")));
+                } else {
+                    return Err(crate::Error::Custom(format!("The search href was auto-detected as geoparquet, but this crate was not compiled with DuckDB support. Either re-install with `-F duckdb` or disable geoparquet auto-detection with `--duckdb false`")));
                 }
             }
         } else {
-            let mut items = Vec::new();
-            while let Some(result) = item_stream.next().await {
-                num_items += 1;
-                items.push(result?);
-                if args
-                    .max_items
-                    .map(|max_items| num_items >= max_items)
-                    .unwrap_or(false)
-                {
-                    break;
+            let client = ApiClient::new(&args.href)?;
+            let item_stream = client.search(search).await?;
+            tokio::pin!(item_stream);
+            let mut num_items = 0;
+            if args.stream {
+                while let Some(result) = item_stream.next().await {
+                    let item: Item = result?;
+                    num_items += 1;
+                    sender.send(item.into()).await?;
+                    if args
+                        .max_items
+                        .map(|max_items| num_items >= max_items)
+                        .unwrap_or(false)
+                    {
+                        break;
+                    }
                 }
+            } else {
+                let mut items = Vec::new();
+                while let Some(result) = item_stream.next().await {
+                    num_items += 1;
+                    items.push(result?);
+                    if args
+                        .max_items
+                        .map(|max_items| num_items >= max_items)
+                        .unwrap_or(false)
+                    {
+                        break;
+                    }
+                }
+                let item_collection = ItemCollection::new(items)?;
+                sender
+                    .send(serde_json::to_value(item_collection)?.into())
+                    .await?;
             }
-            let item_collection = ItemCollection::new(items)?;
-            sender
-                .send(serde_json::to_value(item_collection)?.into())
-                .await?;
         }
         Ok(())
     }
