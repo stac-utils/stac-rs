ab577151ebe5c45bf0059d90b6c370bd54818ad7
Pete Gadomski <pete.gadomski@gmail.com>
2024-12-05 08:19:37 -0700

    fix(pgstac): skip serializing better

--- crates/pgstac/src/page.rs
+++ crates/pgstac/src/page.rs
@@ -11,27 +11,29 @@ pub struct Page {
     pub features: Vec<Item>,
 
     /// The next id.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub next: Option<String>,
 
     /// The previous id.
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub prev: Option<String>,
 
     /// The search context.
     ///
     /// This was removed in pgstac v0.9
-    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub context: Option<Context>,
 
     /// The number of values returned.
     ///
     /// Added in pgstac v0.9
-    #[serde(rename = "numberReturned")]
+    #[serde(rename = "numberReturned", skip_serializing_if = "Option::is_none")]
     pub number_returned: Option<usize>,
 
     /// Links
     ///
     /// Added in pgstac v0.9
-    #[serde(default)]
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub links: Vec<Link>,
 
     /// Additional fields.
