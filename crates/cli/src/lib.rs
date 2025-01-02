a90993c9058b1b3c411a3e64223f9a51eca69450
Pete Gadomski <pete.gadomski@gmail.com>
2025-01-02 08:10:28 -0700

    fix: remove duckdb import in cli

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -54,8 +54,6 @@ pub use {args::Args, error::Error, value::Value};
 /// Crate-specific result type.
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[cfg(feature = "duckdb")]
-use duckdb as _;
 use tracing_subscriber as _;
 
 #[cfg(test)]
