eb404cf095247e716609a1b44efd47b08fd9f6c2
Jonathan Davidson <46798391+jrjdavidson@users.noreply.github.com>
2025-02-04 08:49:21 +1300

    add object-store-aws test (#619)
    small change to add object-store-aws test, also add example on how to
    use keys.
    
    ---------
    
    Co-authored-by: Pete Gadomski <pete.gadomski@gmail.com>

--- crates/core/src/format.rs
+++ crates/core/src/format.rs
@@ -136,12 +136,14 @@ impl Format {
     /// # Examples
     ///
     /// ```no_run
-    /// use stac::{Item, Format};
+    /// use stac::{Catalog, Format};
     ///
     /// #[cfg(feature = "object-store-aws")]
     /// {
     /// # tokio_test::block_on(async {
-    ///     let item: Item = stac::io::get_opts("s3://bucket/item.json", [("aws_access_key_id", "...")]).await.unwrap();
+    ///     let catalog: Catalog = stac::io::get_opts("s3://nz-elevation/catalog.json",
+    ///         [("skip_signature", "true"), ("region", "ap-southeast-2")],
+    ///     ).await.unwrap();
     /// # })
     /// }
     /// ```
--- crates/core/tests/examples.rs
+++ crates/core/tests/examples.rs
@@ -13,3 +13,16 @@ fn v1_1_0(#[files("../../spec-examples/v1.1.0/**/*.json")] path: PathBuf) {
     let value: Value = stac::read(path.to_str().unwrap()).unwrap();
     value.validate().unwrap();
 }
+
+#[test]
+#[cfg(feature = "object-store-aws")]
+fn read_from_s3() {
+    tokio_test::block_on(async {
+        stac::io::get_opts::<stac::Catalog, _, _, _>(
+            "s3://nz-elevation/catalog.json",
+            [("skip_signature", "true"), ("region", "ap-southeast-2")],
+        )
+        .await
+        .unwrap();
+    });
+}
