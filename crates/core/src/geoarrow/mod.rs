f8a75bd34d422e4bf52a0891720b5d1a4e332503
Kyle Barron <kyle@developmentseed.org>
2025-04-18 11:54:11 -0400

    cleanup

--- crates/core/src/geoarrow/mod.rs
+++ crates/core/src/geoarrow/mod.rs
@@ -3,18 +3,20 @@
 pub mod json;
 
 use crate::{Error, ItemCollection, Result};
-use arrow_array::RecordBatchReader;
-use arrow_array::{GenericByteArray, RecordBatch, cast::AsArray, types::GenericBinaryType};
+use arrow_array::{OffsetSizeTrait, RecordBatchIterator, RecordBatchReader};
+use arrow_array::{RecordBatch, cast::AsArray};
 use arrow_json::ReaderBuilder;
-use arrow_schema::{DataType, Field, SchemaBuilder, TimeUnit};
+use arrow_schema::{DataType, Field, SchemaBuilder, SchemaRef, TimeUnit};
 use geo_types::Geometry;
-use geoarrow::{
-    ArrayBase,
-    array::{GeometryBuilder, NativeArrayDyn, WKBArray},
-    datatypes::NativeType,
-    table::Table,
+use geoarrow_array::array::{WkbArray, from_arrow_array};
+use geoarrow_array::builder::{
+    GeometryBuilder, GeometryCollectionBuilder, LineStringBuilder, MultiLineStringBuilder,
+    MultiPointBuilder, MultiPolygonBuilder, PointBuilder, PolygonBuilder, WkbBuilder,
 };
-use geoarrow_schema::{GeometryType, Metadata};
+use geoarrow_array::cast::AsGeoArrowArray;
+use geoarrow_array::error::GeoArrowError;
+use geoarrow_array::{ArrayAccessor, GeoArrowArray, GeoArrowType};
+use geoarrow_schema::{GeometryType, Metadata, WkbType};
 use serde_json::{Value, json};
 use std::sync::Arc;
 
@@ -34,6 +36,25 @@ pub const VERSION_KEY: &str = "geoparquet:version";
 
 /// The stac-geoparquet version.
 pub const VERSION: &str = "1.0.0";
+
+pub struct Table {
+    batches: Vec<RecordBatch>,
+    schema: SchemaRef,
+}
+
+impl Table {
+    pub fn schema(&self) -> &SchemaRef {
+        &self.schema
+    }
+
+    pub fn into_reader(self) -> impl RecordBatchReader {
+        RecordBatchIterator::new(self.batches.into_iter().map(Ok), self.schema)
+    }
+
+    pub fn into_inner(self) -> (Vec<RecordBatch>, SchemaRef) {
+        (self.batches, self.schema)
+    }
+}
 
 /// Converts an [ItemCollection] to a [Table].
 ///
@@ -198,12 +219,13 @@ pub fn with_native_geometry(
 ) -> Result<RecordBatch> {
     if let Some((index, _)) = record_batch.schema().column_with_name(column_name) {
         let geometry_column = record_batch.remove_column(index);
-        let binary_array: GenericByteArray<GenericBinaryType<i32>> =
-            geometry_column.as_binary::<i32>().clone();
-        let wkb_array = WKBArray::new(binary_array, Default::default());
-        let geometry_array = geoarrow::io::wkb::from_wkb(
+        let wkb_array = WkbArray::new(
+            geometry_column.as_binary::<i32>().clone(),
+            Default::default(),
+        );
+        let geometry_array = from_wkb(
             &wkb_array,
-            NativeType::Geometry(GeometryType::new(
+            GeoArrowType::Geometry(GeometryType::new(
                 geoarrow_schema::CoordType::Interleaved,
                 Metadata::default().into(),
             )),
@@ -211,7 +233,7 @@ pub fn with_native_geometry(
         )?;
         let mut columns = record_batch.columns().to_vec();
         let mut schema_builder = SchemaBuilder::from(&*record_batch.schema());
-        schema_builder.push(geometry_array.extension_field());
+        schema_builder.push(geometry_array.data_type().to_field("geometry", true));
         let schema = schema_builder.finish();
         columns.push(geometry_array.to_array_ref());
         record_batch = RecordBatch::try_new(schema.into(), columns)?;
@@ -223,13 +245,10 @@ pub fn with_native_geometry(
 pub fn with_wkb_geometry(mut record_batch: RecordBatch, column_name: &str) -> Result<RecordBatch> {
     if let Some((index, field)) = record_batch.schema().column_with_name(column_name) {
         let geometry_column = record_batch.remove_column(index);
-        let wkb_array = geoarrow::io::wkb::to_wkb::<i32>(&NativeArrayDyn::from_arrow_array(
-            &geometry_column,
-            field,
-        )?);
+        let wkb_array = to_wkb::<i32>(from_arrow_array(&geometry_column, field)?.as_ref())?;
         let mut columns = record_batch.columns().to_vec();
         let mut schema_builder = SchemaBuilder::from(&*record_batch.schema());
-        schema_builder.push(wkb_array.extension_field());
+        schema_builder.push(wkb_array.data_type().to_field("geometry", true));
         let schema = schema_builder.finish();
         columns.push(wkb_array.to_array_ref());
         record_batch = RecordBatch::try_new(schema.into(), columns)?;
@@ -255,12 +274,107 @@ pub fn add_wkb_metadata(mut record_batch: RecordBatch, column_name: &str) -> Res
     Ok(record_batch)
 }
 
+/// Parse a [WKBArray] to a GeometryArray with GeoArrow native encoding.
+///
+/// NOTE(Kyle): the refactored geoarrow-* crates don't yet have a public API for parsing WKB, so we
+/// vendor code here for now. This comes from the internals of geoarrow-geoparquet:
+/// https://github.com/geoarrow/geoarrow-rs/blob/0951163e3e8724a528c2f680a4158106197e7a32/rust/geoarrow-geoparquet/src/reader/parse.rs#L185-L244
+///
+/// This supports either ISO or EWKB-flavored data.
+///
+/// The returned array is guaranteed to have exactly the type of `target_type`.
+///
+/// `GeoArrowType::Rect` is currently not allowed.
+fn from_wkb<O: OffsetSizeTrait>(
+    arr: &WkbArray<O>,
+    target_type: GeoArrowType,
+    prefer_multi: bool,
+) -> Result<Arc<dyn GeoArrowArray>> {
+    use GeoArrowType::*;
+
+    let geoms = arr
+        .iter()
+        .map(|x| x.transpose())
+        .collect::<std::result::Result<Vec<Option<_>>, _>>()?;
+
+    match target_type {
+        Point(typ) => {
+            let builder = PointBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        LineString(typ) => {
+            let builder = LineStringBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        Polygon(typ) => {
+            let builder = PolygonBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        MultiPoint(typ) => {
+            let builder = MultiPointBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        MultiLineString(typ) => {
+            let builder = MultiLineStringBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        MultiPolygon(typ) => {
+            let builder = MultiPolygonBuilder::from_nullable_geometries(&geoms, typ)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        GeometryCollection(typ) => {
+            let builder =
+                GeometryCollectionBuilder::from_nullable_geometries(&geoms, typ, prefer_multi)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        Rect(_) => {
+            Err(GeoArrowError::General(format!("Unexpected data type {:?}", target_type,)).into())
+        }
+        Geometry(typ) => {
+            let builder = GeometryBuilder::from_nullable_geometries(&geoms, typ, prefer_multi)?;
+            Ok(Arc::new(builder.finish()))
+        }
+        _ => todo!("Handle target WKB/WKT in `from_wkb`"),
+    }
+}
+
+/// Convert to WKB
+fn to_wkb<O: OffsetSizeTrait>(arr: &dyn GeoArrowArray) -> Result<WkbArray<O>> {
+    use GeoArrowType::*;
+    match arr.data_type() {
+        Point(_) => impl_to_wkb(arr.as_point()),
+        LineString(_) => impl_to_wkb(arr.as_line_string()),
+        Polygon(_) => impl_to_wkb(arr.as_polygon()),
+        MultiPoint(_) => impl_to_wkb(arr.as_multi_point()),
+        MultiLineString(_) => impl_to_wkb(arr.as_multi_line_string()),
+        MultiPolygon(_) => impl_to_wkb(arr.as_multi_polygon()),
+        Geometry(_) => impl_to_wkb(arr.as_geometry()),
+        GeometryCollection(_) => impl_to_wkb(arr.as_geometry_collection()),
+        Rect(_) => impl_to_wkb(arr.as_rect()),
+        Wkb(_) => impl_to_wkb(arr.as_wkb::<i32>()),
+        LargeWkb(_) => impl_to_wkb(arr.as_wkb::<i64>()),
+        Wkt(_) => impl_to_wkb(arr.as_wkt::<i32>()),
+        LargeWkt(_) => impl_to_wkb(arr.as_wkt::<i64>()),
+    }
+}
+
+fn impl_to_wkb<'a, O: OffsetSizeTrait>(geo_arr: &'a impl ArrayAccessor<'a>) -> Result<WkbArray<O>> {
+    let metadata = geo_arr.data_type().metadata().clone();
+
+    let geoms = geo_arr
+        .iter()
+        .map(|x| x.transpose())
+        .collect::<std::result::Result<Vec<_>, _>>()?;
+    let wkb_type = WkbType::new(metadata);
+    Ok(WkbBuilder::from_nullable_geometries(geoms.as_slice(), wkb_type).finish())
+}
+
 // We only run tests when the geoparquet feature is enabled so that we don't
 // have to add geoarrow as a dev dependency for all builds.
 #[cfg(all(test, feature = "geoparquet"))]
 mod tests {
     use crate::{Item, ItemCollection};
-    use geoarrow::io::parquet::GeoParquetRecordBatchReaderBuilder;
+    use geoarrow_geoparquet::GeoParquetRecordBatchReaderBuilder;
     use std::fs::File;
 
     #[test]
@@ -277,8 +391,7 @@ mod tests {
             .unwrap()
             .build()
             .unwrap();
-        let table = reader.read_table().unwrap();
-        let item_collection = super::from_table(table).unwrap();
+        let item_collection = super::from_record_batch_reader(reader).unwrap();
         assert_eq!(item_collection.items.len(), 1);
     }
 
@@ -286,14 +399,14 @@ mod tests {
     fn roundtrip() {
         let item: Item = crate::read("examples/simple-item.json").unwrap();
         let table = super::to_table(vec![item]).unwrap();
-        let _ = super::from_table(table).unwrap();
+        let _ = super::from_record_batch_reader(table.into_reader()).unwrap();
     }
 
     #[test]
     fn roundtrip_with_missing_asset() {
         let items: ItemCollection = crate::read("data/two-sentinel-2-items.json").unwrap();
         let table = super::to_table(items).unwrap();
-        let _ = super::from_table(table).unwrap();
+        let _ = super::from_record_batch_reader(table.into_reader()).unwrap();
     }
 
     #[test]
